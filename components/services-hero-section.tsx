907796bbc9689fa8e86c5c23b7f18fee9f83495f
segun-aderinola <segunjames554@gmail.com>
2025-07-12 14:56:15 +0100

    update all

--- components/services-hero-section.tsx
+++ components/services-hero-section.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { Calendar } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
 import { useEffect, useState } from "react"
 
 export function ServicesHeroSection() {
@@ -49,21 +50,15 @@ export function ServicesHeroSection() {
                 isVisible ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
               }`}
             >
-              {/* <Button
-                size="lg"
-                className="bg-primary hover:bg-blue-700 text-white px-8 py-4 text-lg font-medium rounded-full transition-all duration-300 hover:scale-105 shadow-lg"
-              >
-                Explore 
-                <Calendar className="w-5 h-5 mr-2" />
-              </Button> */}
-
-              <Button
-                variant="outline"
-                size="lg"
-                className="border-2 border-white text-black hover:bg-black hover:text-white px-8 py-4 text-md font-medium rounded-full bg-white transition-all duration-300 hover:scale-105"
-              >
-                Read About Us
-              </Button>
+              <Link href="/about">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="border-2 border-white text-black hover:bg-black hover:text-white px-8 py-4 text-md font-medium rounded-full bg-white transition-all duration-300 hover:scale-105"
+                >
+                  Read About Us
+                </Button>
+              </Link>
             </div>
           </div>
         </div>
